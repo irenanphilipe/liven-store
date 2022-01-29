212dd73f1931354936084d40a93d0bd7cdf03b9c
Renan Pantoja <renanprpantoja@gmail.com>
2022-01-29 10:50:04 -0400

    Chamada inicial da Api

---
+++ src/api/Api.tsx
@@ -0,0 +1,12 @@
+export type CartItemType = {
+  id: number;
+  category: string;
+  description: string;
+  image: string;
+  price: number;
+  title: string;
+  amount: number;
+};
+
+const getProducts = async (): Promise<CartItemType[]> =>
+  await (await fetch('https://fakestoreapi.com/products')).json();
