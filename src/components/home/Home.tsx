9b4bddeffb7d3484c9772749604377b660926804
Renan Pantoja <renanprpantoja@gmail.com>
2022-01-29 20:11:27 -0400

    Componente Item

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,9 +1,11 @@
 import { useState } from 'react';
 import { useQuery } from 'react-query';
 //componentes
+import Item from '../item/Item';
 import { getProducts, CartItemType } from '../../api/Api';
 import LinearProgress from '@material-ui/core/LinearProgress';
-
+import Grid from '@material-ui/core/Grid';
+import { Wrapper } from '../../App.styles';
 
 function Home() {
   const { data, isLoading, error } = useQuery<CartItemType[]>(
@@ -12,11 +14,21 @@ function Home() {
   );
   console.log(data);
 
+  const handleAddToCart = (clickedItem: CartItemType) => null;
+
   if (isLoading) return <LinearProgress />;
   if (error) return <div>Algo deu errado...</div>;
 
   return(
-    <h1>Home</h1>
+    <Wrapper>
+      <Grid container spacing={3}>
+        {data?.map(item => (
+          <Grid item key={item.id} xs={12} sm={4}>
+            <Item item={item} handleAddToCart={handleAddToCart} />
+          </Grid>
+        ))}
+      </Grid>
+    </Wrapper>
   );
 }
 
